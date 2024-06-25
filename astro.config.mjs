2b22ec9fb0017a99e8eca674a1b2116c0cdc0ee7
vincent87720 <vincent87720@hotmail.com>
2024-06-26 01:46:36 +0800

    Feat: RWD

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://katsuobushi87720.github.io',
+    site: 'https://vincent87720.github.io',
     outDir: './docs',
     trailingSlash: "never",
     build: {
